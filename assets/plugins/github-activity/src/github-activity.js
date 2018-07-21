32a7ba7ee8331267749b09f75927b62be3258c2f
Vikram Kumar <vikramk9852@gmail.com>
2018-07-21 09:50:26 +0530

    Update github-activity.js

--- assets/plugins/github-activity/src/github-activity.js
+++ assets/plugins/github-activity/src/github-activity.js
@@ -263,7 +263,7 @@ var GitHubActivity = (function() {
         activity = Mustache.render(templates.EventsNotFound, { username: options.username });
       } else {
         var limit = options.limit != 'undefined' ? parseInt(options.limit, 10) : null;
-        activity = methods.getActivityHTML(output, limit);
+        activity = methods.getActivityHTML(output, 10);
       }
       methods.renderIfReady(selector, header, activity);
     });
