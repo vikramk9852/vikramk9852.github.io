f700d1fd2094f50b0177431327d3e7872404f4d8
Vikram Kumar <vikramk9852@gmail.com>
2018-07-21 08:41:26 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,7 +55,7 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar("#github-graph", "vikramk9852");
+    GitHubCalendar(".github-graph", "vikramk9852");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
